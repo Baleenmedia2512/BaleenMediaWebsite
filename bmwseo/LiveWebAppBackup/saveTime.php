09dc08035da3be1f47cd3b303137a98146482043
siva-12443 <siva12443@outlook.com>
2025-07-11 09:53:41 +0530

    bmwseo file removed

--- bmwseo/LiveWebAppBackup/saveTime.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-ob_start();
-
-$_SESSION['callTime']=time();
-echo '1';
-exit;
-
-?>
--- bmwseo/TestWellness/Orders/API/db_test.php
+++
@@ -1,3 +0,0 @@
-<?php
-  $mysqli = new mysqli("www.baleenmedia.com","baleeed5_web_adm","admin123$","baleeed5_kiruba_demo","3306");
-?>
